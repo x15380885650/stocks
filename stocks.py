c61747edd8e58d6cc2fa7985c0e21626954350b0
chenggang <chenggang@nj.iscas.ac.cn>
2023-01-18 10:48:39 +0800

    add

--- stocks.py
+++ stocks.py
@@ -42,7 +42,7 @@ def cond_2(data, half_count):   # 近期最高点，且涨幅在0-10
     latest_some_days_max_price = get_max_high_price(data[-1:])
     if latest_some_days_max_price == next_half_max_price:
         i_ratio = ((latest_some_days_max_price-last_half_max_price)/last_half_max_price)*100
-        if 0 <= i_ratio <= 10:
+        if 0 <= i_ratio <= 20:
             return True
     return False
 
@@ -54,9 +54,9 @@ def get_max_high_price(data):
             max_price = float(price)
     return max_price
 
+
 def is_red(one_data):
     return float(one_data['close']) > float(one_data['open'])
-
 
 
 def run():
@@ -96,19 +96,21 @@ def run():
         if trade_status == '0':
             continue
         latest_close_price = float(data['close'].iloc[-1])
-        if latest_close_price < 10 or latest_close_price > 25:
+        if latest_close_price < 0 or latest_close_price > 25:
             continue
 
         red = is_red(data.iloc[-1])
         if not red:
             continue
 
-        # cond_1_ok = cond_1(data[-5:], min_max_day=2)
-        # if not cond_1_ok:
-        #     continue
         cond_2_ok = cond_2(data, half_count)
         if not cond_2_ok:
             continue
+
+        cond_1_ok = cond_1(data[-5:], min_max_day=1)
+        if not cond_1_ok:
+            continue
+
         print(code)
     bs.logout()
 
