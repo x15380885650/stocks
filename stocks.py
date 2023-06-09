60e5582494b6b91fe0e9a6fb0ce3b99a2cbc39ce
chenggang <chenggang@nj.iscas.ac.cn>
2023-06-09 09:57:21 +0800

    add

--- stocks.py
+++ stocks.py
@@ -266,7 +266,7 @@ def run():
         if trade_status == '0':
             continue
         latest_close_price = float(data['close'].iloc[-1])
-        if latest_close_price < 5 or latest_close_price > 25:
+        if latest_close_price < 5 or latest_close_price > 30:
             continue
         # if latest_close_price > 40:
         #     continue
