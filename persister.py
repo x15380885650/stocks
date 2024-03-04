ae19ecb0bda3be4e2b0dcd4a88421e8d780f4a28
chenggang <chenggang@nj.iscas.ac.cn>
2024-03-04 11:15:24 +0800

    add

--- persister.py
+++ persister.py
@@ -4,15 +4,7 @@ from redis import StrictRedis
 class Persister(object):
     def __init__(self, key_prefix):
         redis_conf = {'host': '127.0.0.1', 'port': 6408, 'password': 'iscas139', 'db': 0}
-        try:
-            print('aaaaaaaaaaaaaa')
-            import time
-            time.sleep(1000)
-            self.redis = StrictRedis(**redis_conf, decode_responses=True)
-        except Exception as e:
-            print(e)
-            import time
-            time.sleep(100)
+        self.redis = StrictRedis(**redis_conf, decode_responses=True)
         self.key_prefix = key_prefix
 
     def save_code_to_monitor(self, date, code):
