d236b3eeaea47f6d5b132f61b62939ca55f4bcaf
chenggang <chenggang@nj.iscas.ac.cn>
2024-03-06 08:56:31 +0800

    add

--- script_manager.py
+++ script_manager.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     while True:
         try:
             script_manager.run()
-            time.sleep(10)
+            time.sleep(30)
         except redis.exceptions.ConnectionError:
             print('redis.exceptions.ConnectionError, sleep: {}'.format(120))
             time.sleep(120)
