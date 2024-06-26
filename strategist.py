b2dc78840d6887f53be6faad5d95a3de7e3be1ee
chenggang <chenggang@nj.iscas.ac.cn>
2024-06-26 14:28:07 +0800

    add

--- strategist.py
+++ strategist.py
@@ -741,7 +741,7 @@ class Strategist(object):
 
         up_num, down_num = self.get_up_and_down_num(latest_target_days_k_line_list)
         up_num_2, down_num_2 = self.get_up_and_down_num_2(latest_target_days_k_line_list)
-        if down_num not in [3, 4] and down_num_2 not in [3, 4]:
+        if down_num not in [4, 5] and down_num_2 not in [4, 5]:
             return False, 'eee'
 
         t_3_k_line = latest_target_days_k_line_list[0]
