8ef09b8ec11bfcaaaa79f1a38c8685ae2b981068
x1538088 <103086072+x15380885650@users.noreply.github.com>
2024-12-03 11:04:21 +0800

    Update notifier_3.py

--- notifier_3.py
+++ notifier_3.py
@@ -4,7 +4,7 @@ from notifier import Notifier
 class ThirdNotifier(Notifier):
     def __init__(self):
         super(ThirdNotifier, self).__init__(key_prefix='monitor_3')
-
+        self.is_trade_time_forbidden = False
 
 if __name__ == '__main__':
     notifier = ThirdNotifier()
